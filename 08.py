1b9bfa1228def88007b623008afdee476006f06e
Katalinabeber <cmbeber@escuelasproa.edu.ar>
2024-08-21 11:16:28 -0300

    Ejercicio 2. 19/08

--- 08.py
+++ 08.py
@@ -1,6 +1,7 @@
 #crear una clase fábrica que tenga los atributos llantas, color y precio; luego crear dos clases
 #más que hereden de fábrica para autos y moto,crear objetos para clases
 #que muestren la cantidad de llantas color y precio de los vehículos en pantalla.
+#Agregar una funcion que determine un descuento del 10% en el precio de llanta mayor a 100.000 pesos
 
 class Fabrica:
     def __init__(self, llantas, color, precio):
@@ -11,12 +12,14 @@ class Fabrica:
     def mostrar_info(self):
         print(f"Llantas: {self.llantas}")
         print(f"Color: {self.color}")
-        print(f"Precio: {self.precio}")
+        print(f"Precio: {self.precio:.2f}")
 
-
-class Auto(Fabrica):
-    def __init__(self, color, precio):
-        super().__init__(4, color, precio)
+    def aplicar_descuento(self):
+        if self.precio > 100000:
+            self.precio *= 0.9
+            print("Se aplicó un descuento del 10%")
+        else:
+            print("No se aplicó descuento")
 
 
 class Moto(Fabrica):
@@ -24,13 +27,22 @@ class Moto(Fabrica):
         super().__init__(2, color, precio)
 
 
+class Carro(Fabrica):
+    def __init__(self, color, precio):
+        super().__init__(4, color, precio)
+
+
 # Crear objetos
-auto = Auto("Rojo", 20000)
-moto = Moto("Azul", 5000)
+moto = Moto("Rojo", 80000)
+carro = Carro("Azul", 150000)
 
-# Mostrar información
-print("Auto:")
-auto.mostrar_info()
-
-print("\nMoto:")
+# Mostrar información y aplicar descuento
+print("Moto:")
 moto.mostrar_info()
+moto.aplicar_descuento()
+moto.mostrar_info()
+
+print("\nCarro:")
+carro.mostrar_info()
+carro.aplicar_descuento()
+carro.mostrar_info()
